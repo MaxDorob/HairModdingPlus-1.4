9b886d8590a8e732046b2f35ee91ae0bad4506d6
MaxDorob <maxdor81@gmail.com>
2024-04-15 19:38:44 +0600

    Icons now include back hair (masks not works for icons)

---
+++ Source/1.5/BackLayerForStylingStation_Patch.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace Shashlichnik.HairModdingPlus
+{
+    [HarmonyPatch(typeof(Widgets),nameof(Widgets.DefIcon))]
+    public static class BackLayerForStylingStation_Patch
+    {
+        public static void Prefix(Rect rect, Def def, float scale, ref Material material)
+        {
+            if (def is HairDef hairDef)
+            {
+                var texture = ContentFinder<Texture2D>.Get($"{hairDef.texPath}_back_south", false) ?? ContentFinder<Texture2D>.Get($"{hairDef.texPath}_south_back", false);
+                if (texture != null)
+                {
+                    Widgets.DrawTextureFitted(rect, texture, scale, material);
+                }
+            }
+        }
+
+    }
+}
