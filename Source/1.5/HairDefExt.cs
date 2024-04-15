ad010c2da7a45e3329a901b816742abf94714e9f
MaxDorob <maxdor81@gmail.com>
2024-04-15 12:49:28 +0600

    Changed RenderNode code

--- Source/1.5/HairDefExt.cs
+++ Source/1.5/HairDefExt.cs
@@ -4,11 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Verse;
 
 namespace Shashlichnik.HairModdingPlus
 {
-    public class HairDefExt : HairDef
+    public class HairDefExt : DefModExtension
     {
-
+        public bool noGraphics = false;
+        public ShaderTypeDef overrideShaderTypeDef;
     }
 }
--- Source/1.5/PawnRenderNode_BackLayerHair.cs
+++ Source/1.5/PawnRenderNode_BackLayerHair.cs
@@ -16,17 +16,22 @@ namespace Shashlichnik.HairModdingPlus
         protected override string TexPathFor(Pawn pawn)
         {
             var hair = pawn.story.hairDef;
-            if (hair is HairDefExt)
-            {
-
-            }
+            if (hair?.texPath == null) return null;
             return $"{hair.texPath}_back";
         }
         static string[] directions = { "south", "north", "east", "west" };
         public override Graphic GraphicFor(Pawn pawn)
         {
+            var hair = pawn.story.hairDef;
+            if (hair == null || pawn.DevelopmentalStage.Baby() || pawn.DevelopmentalStage.Newborn()) return null;
+            var hairExt = hair.GetModExtension<HairDefExt>();
+            if (hairExt != null && hairExt.noGraphics) return null;
             if (!directions.Any(x => ContentFinder<Texture2D>.Get($"{TexPathFor(pawn)}_{x}", false) != null)) return null;
-            return base.GraphicFor(pawn);
+            string texPath = TexPathFor(pawn);
+            if (texPath == null) return null;
+            ShaderTypeDef overrideShaderTypeDef = hairExt?.overrideShaderTypeDef ?? hair.overrideShaderTypeDef;
+            return GraphicFor(texPath, overrideShaderTypeDef, ColorFor(pawn));
         }
+        public virtual Graphic GraphicFor(string texPath, ShaderTypeDef overrideShaderTypeDef, Color color) => GraphicDatabase.Get<Graphic_Multi>(texPath, overrideShaderTypeDef?.Shader ?? ShaderDatabase.CutoutHair, Vector2.one, color);
     }
 }
