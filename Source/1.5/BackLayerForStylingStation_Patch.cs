64bc4eae728e7ebaf39551d34615c73e7d7ba298
MaxDorob <maxdor81@gmail.com>
2024-09-15 00:05:54 +0600

    Styling station patch

--- Source/1.5/BackLayerForStylingStation_Patch.cs
+++ Source/1.5/BackLayerForStylingStation_Patch.cs
@@ -14,15 +14,32 @@ namespace Shashlichnik.HairModdingPlus
     [HarmonyPatch(typeof(Widgets),nameof(Widgets.DefIcon))]
     public static class BackLayerForStylingStation_Patch
     {
-        static Dictionary<HairDef, Texture2D> textures = new Dictionary<HairDef, Texture2D>();
+        static Dictionary<HairDef, Texture2D> backTextures = new Dictionary<HairDef, Texture2D>();
+        static Dictionary<HairDef, Texture2D> frontTextures = new Dictionary<HairDef, Texture2D>();
         public static void Prefix(Rect rect, Def def, float scale, ref Material material)
         {
             if (def is HairDef hairDef)
             {
-                if (!textures.TryGetValue(hairDef, out var texture))
+                if (!backTextures.TryGetValue(hairDef, out var texture))
                 {
                     texture = ContentFinder<Texture2D>.Get($"{hairDef.texPath}_back_south", false) ?? ContentFinder<Texture2D>.Get($"{hairDef.texPath}_south_back", false);
-                    textures.Add(hairDef, texture);
+                    backTextures.Add(hairDef, texture);
+                }
+
+                if (texture != null)
+                {
+                    Widgets.DrawTextureFitted(rect, texture, scale, material);
+                }
+            }
+        }
+        public static void Postfix(Rect rect, Def def, float scale, ref Material material)
+        {
+            if (def is HairDef hairDef)
+            {
+                if (!frontTextures.TryGetValue(hairDef, out var texture))
+                {
+                    texture = ContentFinder<Texture2D>.Get($"{hairDef.texPath}_front_south", false);
+                    frontTextures.Add(hairDef, texture);
                 }
 
                 if (texture != null)
